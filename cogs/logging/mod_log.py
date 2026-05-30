8d83f5efdf9e03373be533a6f8aaecb0c4cf124b
Flemmli97 <Flemmli97@users.noreply.github.com>
2026-05-30 03:55:20 +0200

    add logging module

---
+++ cogs/logging/mod_log.py
@@ -0,0 +1,107 @@
+from datetime import datetime
+
+import discord
+from discord.ext import commands
+
+from utils.bot import EMBED_COLOR, Bot
+
+'''
+Note: message logging only works for new messages with the bot
+'''
+
+
+class ModLog(commands.Cog):
+
+    def __init__(self, bot: Bot):
+        self.bot = bot
+        self.logger = self.bot.logger.getChild("Moderation")
+
+    @staticmethod
+    def message_link(message: discord.Message):
+        return f"https://discord.com/channels/{message.guild.id}/{message.channel.id}/{message.id}"
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before: discord.Message, after: discord.Message):
+        if before.author.bot:
+            return
+        desc = f"**Message by {before.author.mention} edited: {ModLog.message_link(before)}**"
+        desc += f"\n== **Before** ==\n{before.content}"
+        desc += f"\n== **After** ==\n{after.content}"
+        await self.bot.send_embed_mod_log(before.guild,
+                                          desc,
+                                          before.author)
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message: discord.Message):
+        if message.author.bot:
+            return
+        desc = f"**Message by {message.author.mention} deleted in {message.channel.mention}**"
+        desc += f"\n{message.content}"
+        await self.bot.send_embed_mod_log(message.guild,
+                                          desc,
+                                          message.author)
+
+    @commands.Cog.listener()
+    async def on_bulk_message_delete(self, messages: list[discord.Message]):
+        non_bots = [message for message in messages if not message.author.bot]
+        if len(non_bots) > 5:
+            channels = set()
+            for message in non_bots:
+                channels.add(message.channel.mention)
+            desc = f"**{len(non_bots)} Messages deleted in {', '.join(channels)}**"
+            await self.bot.send_embed_mod_log(non_bots[0].guild,
+                                              desc,
+                                              message.author)
+            return
+        for message in non_bots:
+            await self.on_message_delete(message)
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before: discord.Member, after: discord.Member):
+        if not before.is_timed_out() and after.is_timed_out():
+            await self.bot.send_embed_mod_log(before.guild,
+                                              f"**{before.mention} got timed out**",
+                                              before)
+
+    @commands.Cog.listener()
+    async def on_audit_log_entry_create(self, entry: discord.AuditLogEntry):
+        match entry.action:
+            case discord.AuditLogAction.kick:
+                source = f' by {entry.user.mention}' if entry.user else ''
+                reason = f' because {entry.reason}' if entry.reason else ''
+                embed = discord.Embed(
+                    description=f"**<@{entry.target.id}> got kicked {source} {reason}**",
+                    timestamp=datetime.now(),
+                    color=EMBED_COLOR
+                )
+                await self.bot.send_mod_log(entry.guild,
+                                            lambda ch: ch.send(embed=embed))
+            case discord.AuditLogAction.ban:
+                # Checking if user was temp or perma banned
+                current = len(self.bot._temp_banning)
+                self.bot._temp_banning = [(guild_id, user_id) for (guild_id, user_id) in self.bot._temp_banning if
+                                          guild_id != entry.guild.id and user_id != entry.target.id]
+                # If not changed this means user was not temp banned
+                if current == len(self.bot._temp_banning):
+                    source = f' by {entry.user.mention}' if entry.user else ''
+                    reason = f' because {entry.reason}' if entry.reason else ''
+                    embed = discord.Embed(
+                        description=f"**<@{entry.target.id}> got banned {source} {reason}**",
+                        timestamp=datetime.now(),
+                        color=EMBED_COLOR
+                    )
+                    await self.bot.send_mod_log(entry.guild,
+                                                lambda ch: ch.send(embed=embed))
+            case discord.AuditLogAction.unban:
+                source = f' by {entry.user.mention}' if entry.user else ''
+                embed = discord.Embed(
+                    description=f"**<@{entry.target.id}> got unbanned {source}**",
+                    timestamp=datetime.now(),
+                    color=EMBED_COLOR
+                )
+                await self.bot.send_mod_log(entry.guild,
+                                            lambda ch: ch.send(embed=embed))
+
+
+async def setup(bot: Bot) -> None:
+    await bot.add_cog(ModLog(bot))
